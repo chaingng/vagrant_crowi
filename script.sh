bc8ffc6c23c842d4217b018a6c614282dbbd872e
Takatomo Honda <chngng0103@gmail.com>
2017-07-14 10:00:03 +0900

    環境変数読み込みを修正

--- script.sh
+++ script.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+# setup ndenv
 git clone https://github.com/riywo/ndenv ~/.ndenv
 echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
 
 git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
+. ~/.bash_profile
 ndenv install v6.11.1
 ndenv global v6.11.1
 ndenv rehash
 
+# setup mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo service mongod start
 
+# setup crowi
 git clone https://github.com/crowi/crowi.git
 cd crowi
 npm i -D node-sass
